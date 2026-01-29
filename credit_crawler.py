d924a623d51c3bb147cae403d93020da04cb7a56
Duc Nguyen <114228174+hogDuc@users.noreply.github.com>
2026-01-29 15:22:30 +0700

    update logger

--- credit_crawler.py
+++ credit_crawler.py
@@ -9,24 +9,41 @@ import re
 from datetime import datetime
 from dotenv import load_dotenv
 
+
 load_dotenv()
 DU_NO_URL = os.getenv('DU_NO_URL')
 IMG_FOLDER = os.getenv('IMG_FOLDER')
 IMG_NAME = os.getenv('IMG_NAME')
-PDF_NAME = os.getenv(PDF_NAME)
+PDF_NAME = os.getenv('PDF_NAME')
 DATA_PATH = os.getenv('DATA_PATH')
+LOG_FOLDER = os.getenv('LOG_FOLDER')
+NOTI_EMAIL = os.getenv('NOTI_EMAIL')
+CUSTOM_PACKAGE = os.getenv('CUSTOM_PACKAGE_PATH')
+
+import sys
+sys.path.append(CUSTOM_PACKAGE)
+from noti_utilities.create_logger import workflow_logger
 
 IMG_SAVE_PATH = os.path.join(IMG_FOLDER, IMG_NAME)
-
 os.makedirs(IMG_FOLDER, exist_ok=True)
+
+# Logger
+log_file = os.path.join(LOG_FOLDER, "credit_crawler.log")
+logger = workflow_logger(
+    name='credit_crawler',
+    log_file=log_file
+).get_logger()
+
 
 def is_up_to_date(old_date, new_date):
 
     if old_date < new_date:
         print(f'New data is available for {new_date.month}/{new_date.year}')
+        logger.info(f'New data is available for {new_date.month}/{new_date.year}')
         return False
     else:
         print('Data is up to date')
+        logger.info('Data is up to date')
         return True
     
 def get_date(df: pd.DataFrame) -> pd.Timestamp:
@@ -94,6 +111,7 @@ current_date = get_date(df)
 
 if not is_up_to_date(old_date=old_date, new_date=current_date):
 
+    logger.info(f'Updating data for {current_date}')
     current_year, current_month = get_year_month(df)
 
     df_processed = df.loc[4:,:].replace("", None).reset_index(drop=True).dropna(how='all', axis=1)
@@ -132,3 +150,4 @@ if not is_up_to_date(old_date=old_date, new_date=current_date):
     updated_data.to_excel(DATA_PATH, index=False)
     
     print('Data updated sucessfully!')
+    logger.info('Data updated sucessfully!')
